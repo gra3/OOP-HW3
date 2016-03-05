5efed9cb114a9574a4f60ddb3baed81b9caa0bab
gra3 <gra3@zips.uakron.edu>
2016-03-04 20:33:43 -0500

    Oops

---
+++ OOP-HW3/Game/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbrahaG.Game
+{
+    class Enemy
+    {
+        public int ArmorRating { get; set; }
+        public int Health { get; set; }
+        public int Damage { get; set; }
+    }
+}
