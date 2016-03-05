bc770e41a632938c9a2b54e6ec038ab8282b9c3f
gra3 <gra3@zips.uakron.edu>
2016-03-04 20:18:54 -0500

    Initial Design Outline

---
+++ OOP-HW3/Game/Map.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbrahaG.Game
+{
+    class Map
+    {
+        //Contains all the tiles for the Map
+        public List<List<Tile>> tileArray;
+
+        //Constructor creates a generic map with same "image", entirely passable
+        public Map(int mapSize)
+        {
+            tileArray = new List<List<Tile>> { };
+
+            for (int i = 0; i < mapSize; i++)
+            {
+                tileArray.Add(new List<Tile> { });
+                for (int j = 0; j < mapSize; j++)
+                {
+                    tileArray[i].Add(new Tile(0, true));
+                }
+            }
+        }
+    }
+}
